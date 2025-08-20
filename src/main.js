6c7ef22b7066635c776d257c1e9ae13511e6cf99
UJDow <lexsnitko@gmail.com>
2025-08-20 16:45:36 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,12 +8,16 @@ function showAuth() {
   setTimeout(() => {
     document.getElementById('authPass').focus();
   }, 100);
+  // Скрываем основной контент
+  document.getElementById('mainContent').style.display = 'none';
 }
 
 function hideAuth() {
   const authDiv = document.getElementById('auth');
   authDiv.style.display = 'none';
   document.body.style.overflow = '';
+  // Показываем основной контент
+  document.getElementById('mainContent').style.display = '';
 }
 
 function getToken() {
