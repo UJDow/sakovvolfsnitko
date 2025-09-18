1917a47eda43d9af17608ca17e1c855077ac36d1
UJDow <lexsnitko@gmail.com>
2025-09-18 11:13:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ let isViewingFromCabinet = false;
 
 let authToken = localStorage.getItem('saviora_jwt') || '';
 
-let currentDreamId = localStorage.getItem('currentDreamId');
-if (currentDreamId === 'null' || !currentDreamId) currentDreamId = null;
+let currentDreamId = null;
 
 /* ====== Палитра блоков ====== */
 const BLOCK_COLORS = ['#ffd966', '#a4c2f4', '#b6d7a8', '#f4cccc', '#d9d2e9'];
