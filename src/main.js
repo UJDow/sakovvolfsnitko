d9cecc46168654fcfdbce9bfaea36d56d631a565
UJDow <lexsnitko@gmail.com>
2025-09-28 15:51:40 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -927,7 +927,7 @@ async globalInterpretation() {
     utils.showToast('Ошибка при итоговом толковании сна', 'error');
   }
   ui.setThinking(false);
-};
+}
 
 ///////////////////////
 // === UI === //
